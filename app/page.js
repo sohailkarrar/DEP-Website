44d5a5fa9d489199899d6303a05d239c2c91b18e
Faraz Ahmad <farazahmad31048@gmail.com>
2024-06-23 11:36:29 +0500

    undoing changes

--- app/page.js
+++ app/page.js
@@ -13,7 +13,6 @@ export default function Home() {
         <div className={styles.mainNameContainer}>
           <h1 className={styles.mainName}>
             A Future of Empowerment Pakistan
-            
           </h1>
         </div>
       </div>
